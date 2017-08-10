8b453c16738700a55cef86fdb1e24420116d5c46
Usuario Alumno <alumno@pxe-server>
2017-08-10 09:14:28 -0300

    Actualizacion

---
+++ TP1/src/Ej1/Main.java
@@ -0,0 +1,48 @@
+package Ej1;
+
+import Utilities.TecladoIn;
+
+/*
+
+ALGORITMO misterio() RETORNA Ø
+(* Este algoritmo ….. *)
+ENTERO x,y
+LEER(x)
+LEER(y)
+x ← x-y
+y ← x+y
+x ← y-x
+ESCRIBIR (“Los valores de x e y son”)
+ESCRIBIR (x)
+ESCRIBIR (y)
+FIN ALGORITMO misterio
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int numero1;
+		int numero2;
+		
+		System.out.println();
+		// Enter firt value
+		numero1 = TecladoIn.readLineInt();
+		// Enter second value
+		numero2 = TecladoIn.readLineInt();
+		
+		
+		// Restar el primer numero al segundo
+		numero1 = numero1 - numero2;
+		
+		// Sumar el primer numero por el segundo numero
+		numero1 = numero1 + numero2;
+		
+		// El primer numero es el segundo meno el prmero
+		numero1 = numero2 - numero1;
+		
+		System.out.println("Los valores de x e y son:");
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+	}
+}
---
+++ TP1/src/Ej3/Main.java
@@ -0,0 +1,31 @@
+package Ej3;
+
+import Utilities.TecladoIn;
+
+/*
+Diseñar un algoritmo que permita leer N números enteros y muestre el
+promedio de los mismos.
+
+Implementar en Java y comprobar la solución mediante una traza.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		int cantidad;
+		
+		// Ingrese la cantidad de numeros que desea leer
+		cantidad = TecladoIn.readLineInt();
+		
+		//Mostrar el promedio
+		Promedio(cantidad);
+		
+	}
+	
+	public static void Promedio(int cantidad){
+		int promedio;
+		// Calcular el promedio
+		for(int i=1; i<cantidad; i++){
+			promedio = cantidad
+		}
+	}
+}
