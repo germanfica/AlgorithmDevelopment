17ed3b3ad4a5f1f6a7804353a644e171632010b1
fica german luis guillermo fai-1673 40614909 <germanfica@pxe-server>
2017-08-17 09:50:45 -0300

    Actulización

--- src/tp3/Ej1.java
+++ src/tp3/Ej1.java
@@ -7,7 +7,7 @@ import utilidades.TecladoIn;
 Diseñar un algoritmo que lea un arreglo de caracteres solicitando al usuario que ingrese letras. El
 algoritmo deberá verificar que los caracteres ingresados son letras. Luego:
 
-	1. Diseñar un módulo que muestre por pantalla los caracteres de las posiciones pares del arreglo de
+	1. Diseñar un módulo:  mostrar los caracteres de las posiciones pares del arreglo de
 	caracteres.
 	
 	2. Diseñar un módulo que muestre por pantalla los caracteres almacenados en el arreglo en orden
@@ -25,9 +25,9 @@ public class Ej1 {
 	public static void mostrarMenu(){
 		System.out.println("----- MENU -----");
 		System.out.println("0- Para finalizar el programa.");
-		System.out.println("1- Mostrar posición.");
-		System.out.println("2- Mostrar carácteres ingresados en orden inverso.");
-		System.out.println("3- Mostrar cantidad de x carácter.");
+		System.out.println("1- Mostrar los carácteres de posiciones pares.");
+		System.out.println("2- Mostrar los carácteres en orden inverso.");
+		System.out.println("3- Mostrar la cantidad que x carácter se repite.");
 	}
 	
 	public static void main(String[] args) {
@@ -39,7 +39,7 @@ public class Ej1 {
 		
 		mostrarMenu();
 		while(i<limite){
-			System.out.println("Ingrese letras:");
+			System.out.println("Ingrese carácter:");
 			letra[i] = TecladoIn.readNonwhiteChar();
 			
 			if(i==i-1)
