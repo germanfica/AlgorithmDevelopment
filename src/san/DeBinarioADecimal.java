f4ab236d111f8da364ab33bf1a61c59cb2542751
germanfica <germafica@gmail.com>
2017-08-23 00:30:08 -0300

    Actualización

--- src/san/DeBinarioADecimal.java
+++ src/san/DeBinarioADecimal.java
@@ -6,16 +6,12 @@ public class DeBinarioADecimal {
 	
 	public static double ConvertirBinarioADecimal(String numeroBinario){
 		double resultado = 0;
-		
-		for (int i = 0; i < numeroBinario.length(); i++) {
-			switch (numeroBinario.charAt(i)) {
-			case '1':
-				resultado = resultado+Math.pow(2, 1);
-				break;
-			default:
-				resultado = resultado+Math.pow(2, 0);
-				break;
-			}
+		int temporal=0;
+
+		for (int i = numeroBinario.length()-1; i >= 0; i--) {
+			if(numeroBinario.charAt(i)=='1')
+				resultado = resultado+Math.pow(2, temporal);
+			temporal=temporal+1;
 		}
 		return resultado;
 	}
