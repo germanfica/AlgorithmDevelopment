d0c73f634947fa9203afd2d15a2170130bfd26a0
germanfica <germanfica@pxe-server>
2018-03-21 10:28:07 -0300

    New update

---
+++ src/practica_marzo_2018/Alumno.java
@@ -0,0 +1,16 @@
+package practica_marzo_2018;
+
+public class Alumno {
+	public String firstName;
+	public String lastName;
+	public int favouriteNumber;
+
+
+public Alumno (String n, String m, int i){
+	firstName = n;
+	lastName = m;
+	favouriteNumber = i;
+}
+
+
+}
---
+++ src/practica_marzo_2018/Principal.java
@@ -0,0 +1,19 @@
+package practica_marzo_2018;
+//import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] arg){
+		
+		Alumno[] alumno = new Alumno[5];
+		
+		// Cartel de saludo
+		System.out.println("Bienvenido.");
+		
+		for(int i = 0; i<alumno.length ; i++)
+			alumno[i]= new Alumno("alumno"+ i,"asd",i);
+		
+		System.out.println("El primer alumno se llama: " + alumno[1].firstName.toString());
+		
+		System.out.println("asd: " + arg[0]);
+	}
+}
