07c02e62adaf7be68be55a12313cb76b5a92b776
germanfica <germanfica@pxe-server>
2018-04-11 11:28:56 -0300

    New update

---
+++ src/test_110418_1118/Alumno.java
@@ -0,0 +1,14 @@
+package test_110418_1118;
+
+public class Alumno {
+	private String name;
+	
+	public void setName(String startName)
+	{
+		name = startName;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
---
+++ src/tp3_ej1_2018/Test.java
@@ -0,0 +1,5 @@
+package tp3_ej1_2018;
+
+public class Test {
+	
+}
