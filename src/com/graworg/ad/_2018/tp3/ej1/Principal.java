2be1d586bc4c43e3dccca138bee9d030d76550d6
germanfica <germafica@gmail.com>
2018-04-13 16:28:27 -0300

    New update

--- src/com/graworg/ad/_2018/tp3/ej1/Principal.java
+++ src/com/graworg/ad/_2018/tp3/ej1/Principal.java
@@ -3,6 +3,8 @@ package com.graworg.ad._2018.tp3.ej1;
 //import java.util.List;
 import java.util.Scanner;
 
+import com.graworg.ad.util.TecladoIn;
+
 /*
  * 1. Retomar el ejercicio visto en clase sobre el TDA Producto. de
  * Tipo de Dato Item de Stock.
@@ -64,6 +66,24 @@ public class Principal {
 	}
 	
 	/**
+	 * Contar la cantidad de productos con un precio unitario menor a un valor dado.
+	 */
+	public static int contarLosProductosConPrecioUnitarioMenorA(float precio)
+	{
+		int contador, índice;
+		contador = 0;
+		
+		for(índice = 0;índice <= productos.length-1;índice++){
+			if(productos[índice].getPrecioUnitario()<precio)
+			{
+				contador = contador + 1;
+			}
+		}
+		
+		return contador;
+	}
+	
+	/**
 	 * Solicitación de los datos para cargar un nuevo producto en específico
 	 */
 	public static void opción1() {
@@ -72,7 +92,7 @@ public class Principal {
 		String descripción;
 		
 		System.out.print("Código: "); código = sc.nextInt();
-		System.out.print("Descripción: "); descripción = sc.next();
+		System.out.print("Descripción: "); descripción = TecladoIn.readLine();
 		System.out.print("Precio unitario: "); precioUnitario = sc.nextFloat();
 		System.out.print("Cantidad de Stock: "); cantidadDeStock = sc.nextInt();
 		
@@ -120,6 +140,25 @@ public class Principal {
 		System.out.println("La cantidad de productos sin stock: " + contador);
 	}
 	
+	/**
+	 * Mostrar la cantidad de productos con un precio unitario menor a un valor dado.
+	 */
+	public static void opción4()
+	{
+		float precio;
+		System.out.print("Valor: "); precio = sc.nextFloat();
+		System.out.println("La cantidad de productos con un precio\n" +
+				"unitario menor a " + precio + " son: " + contarLosProductosConPrecioUnitarioMenorA(precio));
+	}
+	
+	/**
+	 * Aumentar a todos los productos un porcentaje de aumento al precio unitario.
+	 */
+	public static void opción5()
+	{
+		
+	}
+	
 	public static void mostrarMenú() {
 		boolean salir;
 		int opción;
@@ -134,7 +173,11 @@ public class Principal {
 					"[0] Salir\n" +
 					"[1] Cargar producto\n" +
 					"[2] Mostrar listado de productos\n" +
-					"[3] Cantidad de productos con stock 0\n"
+					"[3] Cantidad de productos con stock 0\n" +
+					"[4] Cantidad de productos con un precio\n" +
+						"unitario menor a un valor dado\n" + 
+					"[5] Aumentar a todos los productos un\n" +
+						"porcentaje de aumento al precio unitario.\n"
 					);
 			
 			opción = sc.nextInt();
@@ -152,7 +195,14 @@ public class Principal {
 			case 3:
 				if(productos.length!=0) { opción3(); } else {System.err.println("Debe cargar al menos un producto."); }
 				break;
+			case 4:
+				if(productos.length!=0) { opción4(); } else {System.err.println("Debe cargar al menos un producto."); }
+				break;
+			case 5:
+				if(productos.length!=0) { opción5(); } else {System.err.println("Debe cargar al menos un producto."); }
+				break;
 			default:
+				System.err.println("Esta opción no existe. Seleccione una de las siguientes opciones: ");
 				break;
 			}
 		}
