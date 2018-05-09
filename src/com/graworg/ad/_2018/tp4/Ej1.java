47653ea5ad7c8f4d64a9908851ca3894e58fcee3
germanfica <germanfica@pxe-server>
2018-05-09 10:48:38 -0300

    New update

---
+++ src/com/graworg/ad/_2018/tp4/Ej1.java
@@ -0,0 +1,23 @@
+package com.graworg.ad._2018.tp4;
+
+/* En una empresa de diseño gráfico se utilizan distintos números para catalogar los colores usados en la
+impresora full color. Cada color tiene asociado un número que funciona como factor para calcular el
+precio, ya que a mayor cantidad de colores a usar en la impresión, mayor es el costo del producto.
+Los colores tienen asignado un código que es un números natural y a medida que se agrega un color a la
+impresión se le asigna el siguiente nro natural. Por ejemplo:
+
+Si los colores usados son: Rojo, verde, azul, naranja, celeste, y verde oscuro, el código asociado es: 1, 2,
+3, 4, 5, 6, respectivamente.
+El factor es: 1 + 2 + 3 + 4 + 5 + 6 = 21.
+El precio base de la impresión (sin aplicar el factor) es $100.
+Entonces el precio final de aplicar el factor es $2100.
+Con esto, para calcular el factor Ud. Solo debe conocer cuántos colores va a usar.*/
+
+public class Ej1 {
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
