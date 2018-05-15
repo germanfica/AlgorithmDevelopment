8cd6cb0d9aeb9791e1eed186b769f96db467c6d6
German Fica <germafica@gmail.com>
2018-05-15 10:36:33 -0300

    New update

--- src/com/graworg/ad/_2018/tp4/Ej1.java
+++ src/com/graworg/ad/_2018/tp4/Ej1.java
@@ -1,11 +1,5 @@
 package com.graworg.ad._2018.tp4;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 import com.graworg.ad.util.Archivo;
@@ -13,6 +7,7 @@ import com.graworg.ad.util.Archivo;
 public class Ej1 {
     private static final String NOMBRE_ARCHIVO = "src/com/graworg/ad/_2018/tp4/Colores.txt"; // Ruta del archivo
 	private static int tamArrgelo = 20;
+	private static Scanner sc;
 	
 	/**
      * Convierte un arreglo a texto plano.
@@ -79,43 +74,6 @@ public class Ej1 {
 	}
 	
 	/**
-	 * (3) Carga a un arreglo de String todos los colores de un archivo determinado.
-	 * 
-	 * @see Ahora suponga que los colores usados son provistos en un archivo de texto.
-	 * Realice la carga de un arreglo de Strings a partir del archivo y muestre el color
-	 * con los códigos numéricos asociados a cada uno.
-	 * @return
-	 */
-	public static String[] leerArchivo() {
-		String[] colores = new String[tamArrgelo]; // Creación del arreglo
- 		int i = 0;
-		
-    	try {
-    		BufferedReader buff = new BufferedReader(new FileReader(NOMBRE_ARCHIVO)); // Obtener los datos que están en el archivo de texto
-    		Scanner sc;
-    		sc = new Scanner(buff); // Le asigno al scanner la tarea para buscar los colores dentro del archivo de texto
-    		
-    		
-    		sc.useDelimiter("\\s*,\\s*"); // Me clasifica los colores cuando encuntra la coma
-    		
-    		// Objetivo: es almacenar los colores uno por uno
-    		while(sc.hasNext()) {
-    			colores[i] = sc.next();
-    			System.out.println("Color: " + colores[i] + " Código: " + colores[i]);
-    		}
-    		buff.close();
-    	}
-    	catch (FileNotFoundException ex) {
-            System.err.println(ex.getMessage() + "\nEl archivo no existe.");
-        }
-        catch (IOException ex) {
-            System.err.println("Error al leer o escribir en el archivo.");
-        }
-		
-		return colores;
-	}
-	
-	/**
 	 * (4) Convierte en mayúscula los colores que están en minúscula de un arrreglo.
 	 * 
 	 * @see Todos los colores usados y recuperados del archivo de texto están en
@@ -124,21 +82,56 @@ public class Ej1 {
 	 * el color dado en minúscula. Probarlo en el algoritmo hecho en 3.
 	 * @param args
 	 */
-	public static String[] pasarArregloAMayúsculas(String[] BDColores) {
-		for(int i = 0; i <= (BDColores.length)-1; i++) {
-			BDColores[i] = BDColores[i].toUpperCase();
+	public static String pasarArregloAMayúsculas(String contenido) {
+		String nuevoContenido = "";
+		
+		sc = new Scanner(contenido); // Le asigno al scanner la tarea para buscar los colores y códigos a la cadena de carácteres
+		
+		sc.useDelimiter("\\s*,\\s*"); // Me clasifica los colores cuando encuntra la coma
+		
+		while(sc.hasNext()) {
+			nuevoContenido = nuevoContenido +  ", " + sc.next().toUpperCase();
 		}
 		
-		return BDColores;
+		return nuevoContenido;
+	}
+	
+	/**
+	 * (3) Leer un determinado archivo de texto
+	 * @see Ahora suponga que los colores usados son provistos en un archivo de texto.
+	 * Realice la carga de un arreglo de Strings a partir del archivo y muestre el
+	 * color con los códigos numéricos asociados a cada uno.
+	 * 
+	 * @param args
+	 */
+	public static void mostrarContenidoDelArchivo() {
+		String contenidoDelArchivo = Archivo.leer(NOMBRE_ARCHIVO);
+		
+		sc = new Scanner(contenidoDelArchivo); // Le asigno al scanner la tarea para buscar los colores y códigos a la cadena de carácteres
+		
+		sc.useDelimiter("\\s*,\\s*"); // Me clasifica los colores cuando encuntra la coma
+		
+		// Objetivo: almacenar los colores uno por uno
+		while(sc.hasNext()) {
+			System.out.println("Color: " + sc.next() + "; Código: " + sc.next());
+		}
 	}
 	
 	public static void main(String[] args) {
 		// Declaración de variables
 		String[] BDColores; // Arreglo de los colores
+		String asd;
 		int precioBase = 100; // $100.-
+		System.out.println("Hola");
 		
-		BDColores = pasarArregloAMayúsculas(leerArchivo()); // (4)
-		Archivo.guardar(coloresToString(BDColores), NOMBRE_ARCHIVO); // Guardar los cambios
+		Archivo.guardar(pasarArregloAMayúsculas(Archivo.leer(NOMBRE_ARCHIVO)), NOMBRE_ARCHIVO);
+		
+		mostrarContenidoDelArchivo();
+		
+		//System.out.println(Archivo.leer(NOMBRE_ARCHIVO));
+		
+		//BDColores = pasarArregloAMayúsculas(Archivo.leer(NOMBRE_ARCHIVO)); // (4)
+		//Archivo.guardar(coloresToString(BDColores), NOMBRE_ARCHIVO); // Guardar los cambios
 		
 		
 		//cantidadDeElementosDelArreglo(coloresDesdeArchivo());
