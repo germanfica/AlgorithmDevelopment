8a2bef6e21890470398eef3b9103bc6920fbdcb5
germanfica <germanfica@pxe-server>
2018-06-06 10:22:46 -0300

    New update

---
+++ src/com/graworg/ad_2018/recursion/DeRepaso.java
@@ -0,0 +1,17 @@
+package com.graworg.ad_2018.recursion;
+
+public class DeRepaso {
+	/*
+	 * 4. Trabajando con números enteros. Diseñar e implementar algoritmos recursivos para:
+	 * 	a. Realizar la división por restas sucesivas.
+	 *  b. Invertir un número.
+	 *  c. Sumar los dígitos de un número.
+	 *  d. Contar cuántos dígitos pares tiene un número
+	 */
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
