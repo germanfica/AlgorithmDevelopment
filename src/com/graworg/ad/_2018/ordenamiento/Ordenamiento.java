ae3c2f13cf5a3fd181cff14d8772c623512ea7c4
German Fica <germafica@gmail.com>
2018-06-12 15:54:26 -0300

    New update

--- src/com/graworg/ad/_2018/ordenamiento/Ordenamiento.java
+++ src/com/graworg/ad/_2018/ordenamiento/Ordenamiento.java
@@ -21,7 +21,25 @@ public class Ordenamiento {
 	}
 	
 	public static void burbujaMejorado(int[] arreglo) {
+		int auxiliar, n, i;
+		boolean ordenado;
 		
+		n = arreglo.length;
+		ordenado = false;
+		i = 0;
+		
+		while(i<=n-1 && ordenado==false) {
+			ordenado = true;
+			
+			for(int j=0;j<=(n-i-2);j++) {
+				if(arreglo[j]>arreglo[j+1]) {
+					ordenado = false;
+					auxiliar = arreglo[j];
+					arreglo[j] = arreglo[j+1];
+					arreglo[j+1] = auxiliar;
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
