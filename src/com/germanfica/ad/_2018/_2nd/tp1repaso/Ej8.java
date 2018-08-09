477115354a68a8e0b06f5fc445a21b8d9c3f2652
germanfica <germanfica@pxe-server>
2018-08-09 09:56:42 -0300

    New update

---
+++ src/com/germanfica/ad/_2018/_2nd/tp1repaso/Ej8.java
@@ -0,0 +1,19 @@
+package com.germanfica.ad._2018._2nd.tp1repaso;
+
+public class Ej8 {
+	/*
+	 * 8) En un evento realizado por la empresa de turismo Argentar se
+	 * hizo una encuesta a algunas de las
+	 * personas que asistieron, se les preguntó: nombre y apellido,
+	 * edad, cantidad de países visitados y estado civil. Las personas
+	 * solteras con edad entre 45 y 62 años podían participar del sorteo
+	 * de un viaje a las Cataratas. En base a la información relevada
+	 * se desea mostrar:
+	 * a) El promedio de edad de los asistentes encuestados
+	 * b) El nombre y apellido de la persona que más países visitó
+	 * c) Por cada persona indicar si podía o no participar del sorteo
+	 * 
+	 * Diseñar el algoritmo principal y los módulos correspondientes.
+	 * Implementar en Java.
+	 */
+}
