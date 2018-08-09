17a461982f36dcdab9d21c1efd246c853a14173a
germanfica <germanfica@pxe-server>
2018-08-09 08:46:16 -0300

    New update

--- src/com/germanfica/ad/_2018/_2nd/tp1repaso/Ej4.java
+++ src/com/germanfica/ad/_2018/_2nd/tp1repaso/Ej4.java
@@ -10,26 +10,33 @@ public class Ej4 {
 	public static int letraOcurrencia(String palabra, char letra) {
 		// Declaración de variables
 		int cantidad;
-		
+
 		// Inicialización de variables
 		cantidad = 0;
 		
 		for(int i=0; i<=palabra.length()-1 ;i++) {
-			if(palabra.toLowerCase().charAt(i)==letra)
+			if(palabra.toLowerCase().charAt(i)==letra){
 				cantidad++;
+			}
 		}
 		
 		return cantidad;
 	}
-	
+	/*
+	 * 4) Diseñar un algoritmo que lea una palabra y cuente la
+	 * cantidad
+	 * de veces que aparece una letra dada
+en la palabra. Implementar en Java y realizar traza para
+verificar la solución.
+	 */
 	public static void main(String[] args) {
 		// Declaración de variables
 		char letra;
 		String palabra;
 		
 		// Inicialización de variables
-		palabra = "Hola";
-		letra = 'c';
+		palabra = "Holo";
+		letra = 'o';
 		
 		System.out.println("La letra " + letra + " se repite " + letraOcurrencia(palabra, letra) + " veces en " + palabra + ".");
 	}
