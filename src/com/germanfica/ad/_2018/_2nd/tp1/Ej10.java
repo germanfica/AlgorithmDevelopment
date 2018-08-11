84036b635ed4450301cb7282f56868ee1d72ef43
German Fica <germafica@gmail.com>
2018-08-11 13:38:11 +0100

    New update

--- src/com/germanfica/ad/_2018/_2nd/tp1/Ej10.java
+++ src/com/germanfica/ad/_2018/_2nd/tp1/Ej10.java
@@ -17,7 +17,7 @@ import com.germanfica.ad.util.Mensaje;
  * se detallan a continuación:
  * 
  * - ALGORITMO calcularPerimetroCuadrado(REAL lado) RETORNA REAL
- * - ALGORITMO calcularAreaRectangulo( REAL lado) RETORNA REAL
+ * - ALGORITMO calcularAreaCuadrado( REAL lado) RETORNA REAL
  * 
  * - ALGORITMO calcularPerimetroRectangulo( REAL lado1, REAL lado2) RETORNA REAL
  * - ALGORITMO calcularAreaRectangulo( REAL lado1, REAL lado2) RETORNA REAL
@@ -37,45 +37,47 @@ public class Ej10 {
 		return sc.nextDouble();
 	}
 	
+	// Caso 1
 	public static double calcularPerimetroCuadrado(double lado) {
-		return 0;
+		return 4*lado;
+	}
+	public static double calcularAreaCuadrado(double lado) {
+		return lado*lado;
 	}
 	
-	public static double calcularAreaRectangulo(double lado) {
-		return 0;
-	}
-	
+	// Caso 2
 	public static double calcularPerimetroRectangulo(double lado1, double lado2) {
 		return 0;
 	}
-	
 	public static double calcularAreaRectangulo(double lado1, double lado2) {
 		return 0;
 	}
 	
+	// Caso 3
 	public static double calcularPerimetroTriangulo(double lado) {
 		return 0;
 	}
-	
 	public static double calcularAreaTriangulo(double lado) {
 		return 0;
 	}
 	
+	// Caso 4
+	public static double calcularPerimetroCirculo(double radio) {
+		return 0;
+	}
 	public static double calcularAreaCirculo(double radio) {
 		return 0;
 	}
 	
-	public static double calcularPerimetroCirculo(double radio) {
-		return 0;
-	}
-	
+	/*
+	 * Mostrar menú
+	 */
 	public static void mostrarMenú() {
-		double num, perímetro, área;
+		double perímetro, área, lado, lado1, lado2, radio;
 		boolean salir;
 		int opción;
 		
 		// Inicializar variables
-		num = 0;
 		perímetro = 0;
 		área = 0;
 		salir = false;
@@ -101,19 +103,41 @@ public class Ej10 {
 				salir = true;
 				break;
 			case 1:
-				num = leerReal();
-				perímetro = calcularPerimetroCuadrado(num);
-				área = calcularAreaRectangulo(num);
+				lado = leerReal();
+				perímetro = calcularPerimetroCuadrado(lado);
+				área = calcularAreaCuadrado(lado);
 				System.out.print(
 						"El perímetro del cuadrado es de: " + perímetro + ".\n" +
 						"El área del cuadrado es de: " + área + ".\n"
 						);
 				break;
 			case 2:
+				lado1 = leerReal();
+				lado2 = leerReal();
+				perímetro = calcularPerimetroRectangulo(lado1, lado2);
+				área = calcularAreaRectangulo(lado1, lado2);
+				System.out.print(
+						"El perímetro del rectángulo es de: " + perímetro + ".\n" +
+						"El área del rectángulo es de: " + área + ".\n"
+						);
 				break;
 			case 3:
+				lado = leerReal();
+				perímetro = calcularPerimetroTriangulo(lado);
+				área = calcularAreaTriangulo(lado);
+				System.out.print(
+						"El perímetro del triángulo es de: " + perímetro + ".\n" +
+						"El área del triángulo es de: " + área + ".\n"
+						);
 				break;
 			case 4:
+				radio = leerReal();
+				perímetro = calcularPerimetroCirculo(radio);
+				área = calcularAreaCirculo(radio);
+				System.out.print(
+						"El perímetro del círculo es de: " + perímetro + ".\n" +
+						"El área del círculo es de: " + área + ".\n"
+						);
 				break;
 			default:
 				System.err.println("Esta opción no está definida. Seleccione una de las siguientes opciones: ");
