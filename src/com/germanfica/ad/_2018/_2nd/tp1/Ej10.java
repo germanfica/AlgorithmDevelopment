37279f761a48956633fd3e0924f827dd12380b93
German Fica <germafica@gmail.com>
2018-08-11 12:16:46 +0100

    New update

---
+++ src/com/germanfica/ad/_2018/_2nd/tp1/Ej10.java
@@ -0,0 +1,76 @@
+package com.germanfica.ad._2018._2nd.tp1;
+
+import java.util.Scanner;
+import com.germanfica.ad.util.Mensaje;
+
+/*
+ * 10) Diseñar en Java un algoritmo para calcular el
+ * ------- perímetro y el área -------------
+ * de diferentes figuras geométricas que cuente con un menú de opciones para
+ * las siguientes figuras:
+ * - Cuadrado.
+ * - Rectángulo.
+ * - Triángulo (suponer equilátero).
+ * - Círculo.
+ * 
+ * Desarrollar también los métodos que debe reutilizar el algoritmo y cuyas signaturas
+ * se detallan a continuación:
+ * 
+ * - ALGORITMO calcularPerimetroCuadrado(REAL lado) RETORNA REAL
+ * - ALGORITMO calcularAreaRectangulo( REAL lado) RETORNA REAL
+ * - ALGORITMO calcularPerimetroRectangulo( REAL lado1, REAL lado2) RETORNA REAL
+ * - ALGORITMO calcularAreaRectangulo( REAL lado1, REAL lado2) RETORNA REAL
+ * - ALGORITMO calcularPerimetroTriangulo( REAL lado) RETORNA REAL
+ * - ALGORITMO calcularAreaTriangulo( REAL lado) RETORNA REAL
+ * - ALGORITMO calcularAreaCirculo( REAL radio) RETORNA REAL
+ * - ALGORITMO calcularPerimetroCirculo( REAL radio) RETORNA REAL
+ */
+public class Ej10 {
+	private static Scanner sc;
+	
+	public static void mostrarMenú() {
+		boolean salir;
+		int opción;
+		
+		// Inicializar variables
+		salir = false;
+		
+		System.out.println(Mensaje.leer().get(0));		
+		while(!salir) {
+			sc = new Scanner(System.in);
+			
+			// Mostrar cartel de opciones
+			System.out.print(
+					"[0] Salir (IMPLEMENTADO)\n" +
+					"[1] Perímetro y área de un cuadrado\n" +
+					"[2] Perímetro y área de un rectángulo\n" +
+					"[3] Perímetro y área de un triángulo\n" +
+					"[4] Perímetro y área de un círculo\n"
+					);
+			
+			// Leer opción del menú principal
+			opción = sc.nextInt();
+			
+			switch (opción) {
+			case 0:
+				salir = true;
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				System.err.println("Esta opción no está definida. Seleccione una de las siguientes opciones: ");
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		mostrarMenú();
+	}
+}
