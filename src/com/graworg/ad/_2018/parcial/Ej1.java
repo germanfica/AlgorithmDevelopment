3e045a8ea3621cb4e5bd0e0f8adb47b02bf4433a
German Fica <germafica@gmail.com>
2018-08-21 12:40:04 -0300

    New update

---
+++ src/com/graworg/ad/_2018/parcial/Ej1.java
@@ -0,0 +1,36 @@
+package com.graworg.ad._2018.parcial;
+
+public class Ej1 {
+	/**
+	 * Determina si un algoritmo es decreciente solo si
+	 * el arreglo ingresado está ordenado y tiene números
+	 * sin repetir.
+	 * 
+	 * @param a arreglo de enteros
+	 * @param n longitud del arreglo
+	 * @return
+	 */
+	public static boolean esDecreciente(int[] a, int n) {
+		boolean aux;
+		
+		if(n==0) {
+			if(a[n+1]<a[n]) {
+				aux = true;
+			}else {
+				aux = false;
+			}
+		}else {
+			aux = esDecreciente(a, n-1);
+		}
+		
+		return aux;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {4,3,2,1};
+		int[] b = {1,2,3,4};
+				
+		System.out.println("Es decreciente el arreglo a? " + esDecreciente(a, a.length-1));
+		System.out.println("Es decreciente el arreglo b? " + esDecreciente(b, b.length-1));
+	}
+}
---
+++ src/com/graworg/ad/_2018/tp4bis/Ej1.java
@@ -0,0 +1,38 @@
+package com.graworg.ad._2018.tp4bis;
+
+/*
+ * 1. Determinar a través de trazas cual es el cálculo realizado por siguiente función:  
+ * 
+ * a) Escribir una función iterativa que cumpla la misma función y realizar
+ * su traza correspondiente.
+ * 
+ * b) En el TP4 anterior, cuáles de los incisos utilizaban este tipo de
+ * cálculo recursivo?
+ * 
+ * c) Si el caso base setea aux = 1, y el caso general en vez de sumar
+ * multiplica, qué estaría calculando?
+ * 
+ * d) En qué puntos del TP4 anterior Ud. ve aplicado el tipo de cálculo propuesto en c)?
+ * 
+ * e) Qué nombre le daría al algoritmo recursivo propuesto en el código? Y en el punto c)? 
+ */
+
+public class Ej1 {
+	
+	public static int func (int n) {
+		//Precondición: n siempre es un entero positivo.
+		int aux;
+		
+		if (n==0) {
+			aux = 0;
+		} else {
+			aux = n + func(n - 1);
+		}
+		
+		return aux; 
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(func(10));;
+	}
+}
