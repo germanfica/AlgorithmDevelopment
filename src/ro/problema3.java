524cd312e31ca6fa33907320eb1dc5f47046d576
German Fica <germafica@gmail.com>
2018-08-21 14:19:43 -0300

    New update

---
+++ src/ro/problema3.java
@@ -0,0 +1,37 @@
+package ro;
+
+import java.util.Scanner;
+
+import xyz.germanfica.ad.util.TecladoIn;
+import xyz.germanfica.matematicas.Paridad;
+
+public class problema3 {
+	
+	public static void mostrarMatriz(int cantFilas, int cantColumn) {
+		int cantPares = 0;
+		for (int i = 1; i <= cantFilas; i++) {
+			String numeros = "";
+			int elem = 0;
+			for (int j = 0; j <= cantColumn-1; j++) {
+				elem = TecladoIn.readInt();
+				numeros=numeros+elem + " ";
+				if(Paridad.esPar(elem))
+					cantPares++;
+			}
+			System.out.print("Fila " + i + ": ");
+			System.out.print(numeros);
+			System.out.println("Pares ingresados: " + cantPares);
+			cantPares = 0;
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		int cantFilas, cantColumn;
+		cantFilas = 3;
+		cantColumn = 4;
+		System.out.println("Bienvenido");
+		
+		mostrarMatriz(cantFilas, cantColumn);
+	}
+}
