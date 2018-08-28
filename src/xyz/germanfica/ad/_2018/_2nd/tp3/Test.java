3ab1a995deacab8c00547649262344d4f7bcb86a
German Fica <germafica@gmail.com>
2018-08-29 00:22:37 +0100

    New update

--- src/xyz/germanfica/ad/_2018/_2nd/tp3/Test.java
+++ src/xyz/germanfica/ad/_2018/_2nd/tp3/Test.java
@@ -3,7 +3,27 @@ package xyz.germanfica.ad._2018._2nd.tp3;
 import xyz.germanfica.util.Utils;
 
 public class Test {
-	public static void main(String[] args) {
-		Utils.tiempoRestante(5);
-	}
+	public static void main(String[] args){
+	    try{
+	      System.out.print("Completed : 0  %");
+	      Thread.currentThread().sleep(100);
+	      for(int i=1; i<=100; i++){
+	        System.out.print("\b\b\b\b"+pad(i)+"%");
+	        Thread.currentThread().sleep(100);
+	      }
+	    }
+	    catch(Exception e){
+	    }
+	    System.out.print("\nDone!!!");
+	  }
+	  public static String pad(int i){
+	    String s = ""+i;
+	    if(s.length()==1){
+	      s += "  ";
+	    }
+	    else if(s.length()==2){
+	      s += " ";
+	    }
+	    return s;
+	  }
 }
