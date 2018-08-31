a0b7af2c235d962e6dbe88fa49a665ea6500dc8b
German Fica <germafica@gmail.com>
2018-08-31 21:33:54 +0100

    New update

--- src/xyz/germanfica/ad/_2018/_2nd/tp3/Ej6.java
+++ src/xyz/germanfica/ad/_2018/_2nd/tp3/Ej6.java
@@ -3,23 +3,40 @@ package xyz.germanfica.ad._2018._2nd.tp3;
 import java.util.Scanner;
 
 import xyz.germanfica.mensaje.Mensaje;
+import xyz.germanfica.util.MatrizEntero;
 
 /*
  * 6. Diseñar un algoritmo para recorrer una matriz de enteros de
  * nxn previamente cargada, mostrando su contenido recorriéndola
  * en espiral:
- * 
  */
 public class Ej6 {
+	private static final String ARCHIVO = "src/xyz/germanfica/ad/_2018/_2nd/tp3/Ej6.txt"; // Ruta del archivo
 	public static Scanner sc;
+	
+	public static int[][] matrizEspiral(int[][] matriz) {
+		// Declaración de variables
+		int[][] matrizEspiral;
+		int cantFilas, cantColumnas;
+		
+		// Inicialización de variables
+		cantFilas = matriz.length;
+		cantColumnas = matriz[0].length;
+		matrizEspiral = new int[cantFilas][cantColumnas];
+		
+		// Cambiar la disposición de los elementos
+		
+		
+		return matrizEspiral;
+	}
+	
 	/*
 	 * Este modulo se encarga de mostrar los carteles del menu
 	 */
 	public static void mostrarCartelDeOpciones() {
 		System.out.print("[0] Salir (IMPLEMENTADO)\n"
-				+ "[1] \n"
-				+ "[2] \n"
-				+ "[3] \n"
+				+ "[1] Mostrar matriz original (IMPLEMENTADO)\n"
+				+ "[2] Mostrar matriz pero recorriéndola en espiral\n"
 				);
 	}
 	
@@ -27,7 +44,7 @@ public class Ej6 {
 	 * Mostrar el menu de la aplicacion
 	 * Nota: los modulos no deben ocupar mas de una pantalla
 	 */
-	public static void mostrarMenu(String[] palabras) {
+	public static void mostrarMenu(int[][] matriz) {
 		boolean salir = false;
 		int opcion;
 		
@@ -44,14 +61,24 @@ public class Ej6 {
 			
 			switch (opcion) {
 			case 0: salir = true; break;
-			// [1]
-			case 1: ;break; // Opcion 1
-			// [2]
+			// [1] Mostrar matriz original
+			case 1: MatrizEntero.mostrar(matriz);break; // Opcion 1
+			// [2] Mostrar matriz pero recorriéndola en espiral
 			case 2: ; break; // Opcion 2
-			// [3]
-			case 3: ; break; // Opcion 3
 			default: System.err.println(Mensaje.leer(1)); break;
 			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		// Declaración de variables
+		int[][] matriz; // Matriz de enteros
+		
+		// Inicilización de variables
+		matriz = MatrizEntero.cargaDesdeArchivo(ARCHIVO);
+		
+		// Mostrar menú
+		mostrarMenu(matriz);
+		
 	}
 }
