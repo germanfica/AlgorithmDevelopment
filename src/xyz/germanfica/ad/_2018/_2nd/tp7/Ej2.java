4d03722e3135abb579c9420559b650e2b8f229a4
German Fica <germafica@gmail.com>
2018-10-27 21:09:30 +0100

    Recursividad 2a

---
+++ src/xyz/germanfica/ad/_2018/_2nd/tp7/Ej2.java
@@ -0,0 +1,25 @@
+package xyz.germanfica.ad._2018._2nd.tp7;
+
+import java.util.Scanner;
+
+public class Ej2 {
+	private static Scanner sc;
+	public static int digito(int n) {
+		int cantDig = 1;
+		
+		if(n<=9 && n>0) {
+			cantDig = 1;
+		}else {
+			cantDig = cantDig + digito(n/10);
+		}
+		
+		return cantDig;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Ingrese la cantidad de dígitos:");
+		sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("La cantidad de dígitos del número " + n + " es: " + digito(n));
+	}
+}
