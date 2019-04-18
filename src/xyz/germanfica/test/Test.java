39b9552a285b5b934f267f6be6ae2432ddcb70c2
German Fica <germafica@gmail.com>
2019-04-18 20:57:48 -0300

    Nuevos archivos

---
+++ src/xyz/germanfica/test/Test.java
@@ -0,0 +1,11 @@
+package xyz.germanfica.test;
+
+import xyz.germanfica.util.Archivo;
+
+public class Test {
+	public static void main(String[] args) {
+		String NOMBRE_ARCHIVO = "src/xyz/germanfica/util/ejemplo.txt";
+		String ejemplo = Archivo.leer(NOMBRE_ARCHIVO);
+		System.out.println(ejemplo);
+	}
+}
