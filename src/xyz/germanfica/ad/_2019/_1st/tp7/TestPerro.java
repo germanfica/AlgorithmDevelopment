6f208fcd061b6b181774be0e36e2decf13b47364
German Fica <germafica@gmail.com>
2019-04-19 01:57:57 -0300

    New update

--- src/xyz/germanfica/ad/_2019/_1st/tp7/TestPerro.java
+++ src/xyz/germanfica/ad/_2019/_1st/tp7/TestPerro.java
@@ -3,15 +3,23 @@ package xyz.germanfica.ad._2019._1st.tp7;
 import xyz.germanfica.util.Archivo;
 
 public class TestPerro {
-	public static void main(String[] args) {
-		String ejemploTxt = "src/xyz/germanfica/util/ejemplo.txt";
-		String ejemplo = Archivo.leer(ejemploTxt).getTexto();
-		System.out.println(ejemplo);
-		
-		String[] stringTDA = Archivo.leer("src/xyz/germanfica/ad/_2019/_1st/tp7/StringTDA.txt").toArray();
-		
-		for (int i = 0; i < stringTDA.length; i++) {
-			System.out.println(stringTDA[0]);
+	
+	public static void mostrarPerros(Perro[] perros) {
+		for (int i = 0; i < perros.length; i++) {
+			System.out.println(perros[i].toString());
 		}
+	}
+	
+	public static void cargaPerros(Perro[] perros) {
+		for (int i = 0; i <= perros.length-1; i++) {
+			perros[i] = Perro.fromString(Archivo.leer("src/xyz/germanfica/ad/_2019/_1st/tp7/StringTDA.txt").toArray()[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Perro[] perros = new Perro[10];
+		
+		cargaPerros(perros);
+		mostrarPerros(perros);
 	}
 }
