46718846e5a36409ce07bb81186c8eeb257a46a6
germanfica <germanfica@pxe-server>
2019-05-13 13:04:02 -0300

    New update

--- src/xyz/germanfica/ad/_2019/_2nd/primer_parcial/Ej1.java
+++ src/xyz/germanfica/ad/_2019/_2nd/primer_parcial/Ej1.java
@@ -1,6 +1,12 @@
 package xyz.germanfica.ad._2019._2nd.primer_parcial;
 
 public class Ej1 {
+	
+	public static String actualizar(String pais, String ciudad, String numero) {
+		String numCelActualizado = "";
+		
+		return numCelActualizado;
+	}
 
 	public static String actualizaNumCel(String numCel) {
 		String pais = "";
@@ -24,20 +30,29 @@ public class Ej1 {
 		}
 
 		numero = numCel;
-
-		if (numero.charAt(0) != '1' && numero.charAt(1) != '5') {
-			numCelActualizado = pais + ciudad + numero;
-		} else {
+		
+		if (numero.charAt(0) == '1' && numero.charAt(1) == '5') {
 			numCelActualizado = "4" + numero.substring(2, numero.length());
+		}else {
+			numCelActualizado = pais + ciudad + numero;
 		}
-
-		System.out.println(pais + " " + ciudad + " " + numero);
 
 		return numCelActualizado;
 	}
 
 	public static void main(String[] args) {
-		String numCel = "+54-0299-155030299";
-		System.out.println(actualizaNumCel(numCel));
+		String numCel1 = "+54-0299-155030299";
+		String numCel2 = "0299-155030299";
+		String numCel3 = "155030299";
+		String numCel4 = "1655030299";
+		String numCel5 = "+54-0299-155030155";
+		String numCel6 = "+54-0299-165030155";
+
+		System.out.println(actualizaNumCel(numCel1));
+		System.out.println(actualizaNumCel(numCel2));
+		System.out.println(actualizaNumCel(numCel3));
+		System.out.println(actualizaNumCel(numCel4));
+		System.out.println(actualizaNumCel(numCel5));
+		System.out.println(actualizaNumCel(numCel6));
 	}
 }
