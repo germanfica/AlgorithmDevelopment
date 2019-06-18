5949efda434679c70efdf5d5fceb477b35afae0f
German Fica <germafica@gmail.com>
2019-06-18 17:59:47 -0300

    Generador id kilometraje Una mejora, ahora el idKm está encriptado con sha-256.

--- src/xyz/germanfica/_2019/_1st/tpfinal/Aleatorio.java
+++ src/xyz/germanfica/_2019/_1st/tpfinal/Aleatorio.java
@@ -1,13 +1,9 @@
 package xyz.germanfica._2019._1st.tpfinal;
 
 public class Aleatorio {
-	/**
-	 * Crear un TDA “Aleatorio” que guarde un número con la siguiente información:
-	 * a. Número aleatorio entero. b. Atributo que diga si tiene dígitos gemelos
-	 * (resolverlo por recursión). c. Atributo que diga si pertenece a la serie de
-	 * Fibonacci (resolverlo por recursión). d. Los metodos observadores deben
-	 * incluir a equals(), toString() y compareTo(). e. Implemente el método
-	 * clonar(), que retorna un nuevo objeto Aleatorio que es copia del objeto
-	 * Aleatorio al que le envió el mensaje.
-	 */
+	private int numAleatorio;
+	private boolean tieneDigGem;
+	private boolean perteneceFibo;
+	
+	
 }
