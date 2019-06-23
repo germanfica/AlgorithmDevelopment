29914f9ed08625751a0f4499c8aaea66d0b26a33
German Fica <germafica@gmail.com>
2019-06-22 22:09:05 -0300

    New update

--- src/xyz/germanfica/_2019/_1st/tpfinal/Aleatorio.java
+++ src/xyz/germanfica/_2019/_1st/tpfinal/Aleatorio.java
@@ -1,9 +1,63 @@
 package xyz.germanfica._2019._1st.tpfinal;
 
+/**
+ * @authors [GERMAN LUIS GUILLERMO FICA, GONZALO DARIO OLMOS]
+ *
+ */
 public class Aleatorio {
-	private int numAleatorio;
+	private int numAleatorio; // La clave
 	private boolean tieneDigGem;
 	private boolean perteneceFibo;
-	
-	
+
+	Aleatorio() {
+		numAleatorio = (int) (Math.random() * 3798);
+		tieneDigGem = false;
+		perteneceFibo = false;
+	}
+
+	// Observadores
+	public int getNumAleatorio() {
+		return numAleatorio;
+	}
+
+	public boolean equals(Aleatorio a) {
+		return numAleatorio == a.numAleatorio;
+	}
+
+	public String toString() {
+		return "{\"numAleatorio\": " + numAleatorio + ", \"tieneDigGem\": " + tieneDigGem + ", \"perteneceFibo\": "
+				+ perteneceFibo + "}";
+	}
+
+	/**
+	 * Si el numAleatorio de la clase es más pequeño al parametro, devuelve -1.
+	 * 
+	 * Si el numAleatorio de la clase es mayor al parametro, retorna 1.
+	 * 
+	 * Si el numAleatorio de la clase es igual al parametro, retorna 0.
+	 * 
+	 * Fuente: https://www.tutorialspoint.com/java/number_compareto.htm
+	 * 
+	 * @return
+	 */
+	public int compareTo(Aleatorio objBuscado) {
+		int resultado;
+
+		if (numAleatorio < objBuscado.numAleatorio) {
+			// menor
+			resultado = -1;
+		} else if (numAleatorio > objBuscado.numAleatorio) {
+			// mayor
+			resultado = 1;
+		} else {
+			// son iguales
+			resultado = 0;
+		}
+		return resultado;
+	}
+
+	// Mofificadores
+
+	// Propias del tipo
+
 }
--- src/xyz/germanfica/_2019/_1st/tpfinal/Principal.java
+++ src/xyz/germanfica/_2019/_1st/tpfinal/Principal.java
@@ -1,8 +1,12 @@
 package xyz.germanfica._2019._1st.tpfinal;
 
 public class Principal {
-	
+
 	public static void main(String[] args) {
-		
+		Aleatorio aleatorio = new Aleatorio();
+		System.out.println(aleatorio.toString());
+		Aleatorio aleBus = new Aleatorio();
+		System.out.println(aleBus.getNumAleatorio());
+		System.out.println(aleatorio.compareTo(aleBus));
 	}
 }
